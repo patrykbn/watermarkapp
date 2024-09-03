e979af5b433e837a1fee3a9167e8b06b41babb84
Patryk Bagan <patrykbn1997@gmail.com>
2024-09-03 19:10:42 +0200

    Watermark App 1.0v

---
+++ app.js
@@ -0,0 +1,109 @@
+const { existsSync } = require('node:fs');
+const Jimp = require('jimp');
+const inquirer = require('inquirer');
+
+const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
+  try {
+    const image = await Jimp.read(inputFile);
+    const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+    const textData = {
+        text: text,
+        alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+        alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+    }
+  image.print(font, 10, 10, textData, image.getWidth(), image.getHeight());
+  await image.quality(100).writeAsync(outputFile);
+    } catch (error) {
+        console.error('Error adding text watermark...');
+    }
+};
+
+//addTextWatermarkToImage('./test.jpg', './test-with-watermark.jpg', 'Hello world');
+
+const addImageWatermarkToImage = async function(inputFile, outputFile, watermarkFile) {
+    try {
+        const image = await Jimp.read(inputFile);
+        const watermark = await Jimp.read(watermarkFile);
+        const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+        const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+  
+        image.composite(watermark, x, y, {
+            mode: Jimp.BLEND_SOURCE_OVER,
+            opacitySource: 0.5,
+        });
+        await image.quality(100).writeAsync(outputFile);
+    } catch(error) {
+        console.error('Error adding image watermark to image...');
+    }
+};
+  
+  //addImageWatermarkToImage('./test.jpg', './test-with-watermark2.jpg', './logo.png');
+const prepareOutputFilename = (filename) => {
+    const [ name, ext ] = filename.split('.');
+    return `${name}-with-watermark.${ext}`;
+};
+
+  const startApp = async () => {
+  try {
+    // Ask if user is ready
+    const answer = await inquirer.prompt([{
+        name: 'start',
+        message: 'Hi! Welcome to "Watermark manager". Copy your image files to `/img` folder. Then you\'ll be able to use them in the app. Are you ready?',
+        type: 'confirm'
+      }]);
+  
+    // if answer is no, just quit the app
+    if(!answer.start) process.exit();
+  
+    // ask about input file and watermark type
+    const options = await inquirer.prompt([{
+      name: 'inputImage',
+      type: 'input',
+      message: 'What file do you want to mark?',
+      default: 'test.jpg',
+    }, {
+      name: 'watermarkType',
+      type: 'list',
+      choices: ['Text watermark', 'Image watermark'],
+    }]);
+
+    const inputFilePath = './img/' + options.inputImage;
+
+    if(!existsSync(inputFilePath)) {
+        console.log('The specified input file does not exist. Please try again');
+        process.exit();
+    }
+
+    if(options.watermarkType === 'Text watermark') {
+        const text = await inquirer.prompt([{
+          name: 'value',
+          type: 'input',
+          message: 'Type your watermark text:',
+          default: 'All rights reserved',
+        }]);
+        options.watermarkText = text.value;
+        await addTextWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), options.watermarkText);
+      }
+      else {
+        const image = await inquirer.prompt([{
+          name: 'filename',
+          type: 'input',
+          message: 'Type your watermark name:',
+          default: 'logo.png',
+        }]);
+        options.watermarkImage = image.filename;
+        const watermarkInputFilePath = './img/' + options.watermarkImage;
+
+        if(!existsSync(watermarkInputFilePath)) {
+            console.log('The specified watermark image does not exist. Please try again.');
+            process.exit();
+        }
+        await addImageWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), './img/' + options.watermarkImage);
+      }
+      console.log('Watermark added successfully!');
+    } catch(error) {
+        console.error('An unexpected error occured...')
+    }
+  }
+  
+  startApp();
